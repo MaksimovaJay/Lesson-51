442f58b4f9e475266112a71bfa4979ae9f876f89
Maksimova Evgeniya <Nikitina_jenya1997@mail.ru>
2023-12-21 23:47:17 +0600

    changed app

--- components-demo/src/App.tsx
+++ components-demo/src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [numbers, setNumbers] = useState([5, 11, 16, 23, 32])
+    function compareNumbers(a, b) {
+        return a - b;
+    }
+    const generation = () => {
+        let newNumbers: number[] = []
+        for (let i = 0; i < 5; i++) {
+            let randomNumber: number = Math.floor(Math.random() * 31) + 5;
+            while (newNumbers.includes(randomNumber)) {
+                 randomNumber = Math.floor(Math.random() * 31) + 5;
+            }
+            newNumbers.push(randomNumber);
+        }
+        newNumbers.sort(compareNumbers)
+        console.log(newNumbers)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        setNumbers(newNumbers);
+    };
+    return (
+        <>
+            <button type={"button"} onClick={generation}>New numbers</button>
+            {numbers.map((circle, index) => (
+                    <div key={index}>{circle}</div>
+                )
+            )}
+        </>
+    )
 }
 
 export default App
