2943e462f3f5838bb4cf001284c376cd1902bd28
Maksimova Evgeniya <Nikitina_jenya1997@mail.ru>
2024-01-16 14:49:44 +0600

    доделала дизайн и круги

--- components-demo/src/App.tsx
+++ components-demo/src/App.tsx
@@ -4,33 +4,37 @@ import './App.css'
 
 
 function App() {
-    const [numbers, setNumbers] = useState([5, 11, 16, 23, 32])
+  const [numbers, setNumbers] = useState([5, 11, 16, 23, 32])
 
-    function compareNumbers(a:number, b:number): number {
-        return a - b;
+  function compareNumbers(a: number, b: number): number {
+    return a - b;
+  }
+
+  const generation = () => {
+    let newNumbers: number[] = []
+    for (let i = 0; i < 5; i++) {
+      let randomNumber: number = Math.floor(Math.random() * 31) + 5;
+      while (newNumbers.includes(randomNumber)) {
+        randomNumber = Math.floor(Math.random() * 31) + 5;
+      }
+      newNumbers.push(randomNumber);
     }
-
-    const generation = () => {
-        let newNumbers: number[] = []
-        for (let i = 0; i < 5; i++) {
-            let randomNumber: number = Math.floor(Math.random() * 31) + 5;
-            while (newNumbers.includes(randomNumber)) {
-                randomNumber = Math.floor(Math.random() * 31) + 5;
-            }
-            newNumbers.push(randomNumber);
-        }
-        newNumbers.sort(compareNumbers)
-        setNumbers(newNumbers);
-    };
-    return (
-        <>
-            <button type={"button"} onClick={generation}>New numbers</button>
-            {numbers.map((circle, index) => (
-                    <Circle key={index} circle={circle}/>
-                )
-            )}
-        </>
-    )
+    newNumbers.sort(compareNumbers)
+    setNumbers(newNumbers);
+  };
+  return (
+    <>
+      <div className="wrapper">
+        <button type={"button"} onClick={generation}>New numbers</button>
+        <div className='wrapperCircle'>
+          {numbers.map((circle, index) => (
+              <Circle key={index} circle={circle}/>
+            )
+          )}
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App
---
+++ components-demo/src/Circle.tsx
@@ -0,0 +1,7 @@
+
+function Circle(props:{circle: number}) {
+    return (
+        <div className='designerCircle'>{props.circle}</div>
+    )
+}
+export default Circle
